a8a66b9204ed25b74b18aeffe5a05c1bdddc3891
Philippe Thiers <philippe.thiers@gmail.com>
2024-09-13 17:05:14 -0300

    fix wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -5,8 +5,38 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
+config.font_size = 10
+config.font = wezterm.font 'CodeNewRoman Nerd Font Mono'
+config.color_scheme = 'catppuccin-macchiato'
 
-config.color_scheme = 'Catppuccin Frappe'
+config.window_frame = {
+    -- The font used in the tab bar.
+    -- Roboto Bold is the default; this font is bundled
+    -- with wezterm.
+    -- Whatever font is selected here, it will have the
+    -- main font setting appended to it to pick up any
+    -- fallback fonts you may have used there.
+    font = wezterm.font { family = 'CodeNewRoman Nerd Font Mono', weight = 'Bold' },
+
+    -- The size of the font in the tab bar.
+    -- Default to 10.0 on Windows but 12.0 on other systems
+    font_size = 10.0,
+
+    -- The overall background color of the tab bar when
+    -- the window is focused
+    active_titlebar_bg = '#333333',
+
+    -- The overall background color of the tab bar when
+    -- the window is not focused
+    inactive_titlebar_bg = '#101421',
+}
+
+config.colors = {
+    tab_bar = {
+        -- The color of the inactive tab bar edge/divider
+        inactive_tab_edge = '#575757',
+    },
+}
 
 -- and finally, return the configuration to wezterm
 return config
