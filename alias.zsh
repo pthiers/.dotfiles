76db5207214240d4ab4fc52d9b2cea3dccb9bae2
Philippe Thiers <philippe.thiers@gmail.com>
2024-09-13 12:59:15 -0300

    primer commit

---
+++ alias.zsh
@@ -0,0 +1,16 @@
+# Group directories first, show icons, and enable file size color scale
+alias ls='exa --group-directories-first --icons --color-scale'
+alias lt='exa --tree --level=2 --icons' # Show in tree view
+alias l='ls -a'                         # Short, all files
+alias ld='l -D'                         # Short, only directories
+alias ll='ls -lbG --git'                # Long, file size prefixes, grid, git status
+alias la='ll -a'                        # Long, all files
+alias lC='la --sort=changed'            # Long, sort changed
+alias lM='la --sort=modified'           # Long, sort modified
+alias lS='la --sort=size'               # Long, sort size
+alias lX='la --sort=extension'          # Long, sort extension
+
+alias pbcopy='xclip -selection clipboard'
+alias pbpaste='xclip -selection clipboard -o'
+
+alias grep='grep --color'
