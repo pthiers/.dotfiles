a6ac08c64b80e3910861f309092eb8610a17a93a
Philippe Thiers <philippe.thiers@gmail.com>
2024-09-13 16:02:58 -0300

    fix git and ls

--- alias.zsh
+++ alias.zsh
@@ -3,7 +3,7 @@ alias ls='exa --group-directories-first --icons --color-scale'
 alias lt='exa --tree --level=2 --icons' # Show in tree view
 alias l='ls -a'                         # Short, all files
 alias ld='l -D'                         # Short, only directories
-alias ll='ls -lbG --git'                # Long, file size prefixes, grid, git status
+alias ll='ls -lb --git'                # Long, file size prefixes, grid, git status
 alias la='ll -a'                        # Long, all files
 alias lC='la --sort=changed'            # Long, sort changed
 alias lM='la --sort=modified'           # Long, sort modified
