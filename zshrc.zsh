83bab9b0a4b137343c6d0c668203a4a2b698cb34
Philippe Thiers <philippe.thiers@gmail.com>
2025-06-17 09:24:20 -0400

    nvm

--- zshrc.zsh
+++ zshrc.zsh
@@ -84,3 +84,8 @@ eval "$(zoxide init --cmd cd zsh)"
 
 # Golang version manager
 [[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"
+
+#Node version manager
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
